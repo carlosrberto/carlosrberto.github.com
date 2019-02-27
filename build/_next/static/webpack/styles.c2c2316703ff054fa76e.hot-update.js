49d7c44fb502600029e40ae11646aabdf9cbc442
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2019-02-27 11:26:59 -0300

    new build

---
+++ build/_next/static/webpack/styles.c2c2316703ff054fa76e.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("styles",{
+
+/***/ "./styles/global.sass":
+/*!****************************!*\
+  !*** ./styles/global.sass ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+module.exports = {"main":"_2uUnJd8bQHRe2nQg8Od8n_","background":"CkOQ3O604NQv8JwdX4lV2","profilePicture":"_3bICw1v0R6CnYIKoj5CA7F","title":"_99mLLACzOQYuI4BvAUsDl","subTitle":"_2ZOf_FMqye2acYka8P4TAG","intro":"_2hY24MjD3Bez6LBC1haw8h","socialLinks":"rwtEf8SI5iVgvm1vnHM4H"};;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1551276591991");
+          });
+      }
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=styles.c2c2316703ff054fa76e.hot-update.js.map
