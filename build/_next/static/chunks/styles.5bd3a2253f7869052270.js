d8663e58bf5e921ae628c522745c50a37a546311
Carlos Roberto Gomes Jr <carlos@andfrankly.com>
2022-02-20 22:27:11 +0100

    update build

--- build/_next/static/chunks/styles.5bd3a2253f7869052270.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{gRm8:function(n,i,o){n.exports={main:"_2uUnJd8bQHRe2nQg8Od8n_",background:"CkOQ3O604NQv8JwdX4lV2",profilePicture:"_3bICw1v0R6CnYIKoj5CA7F",title:"_99mLLACzOQYuI4BvAUsDl",subTitle:"_2ZOf_FMqye2acYka8P4TAG",intro:"_2hY24MjD3Bez6LBC1haw8h",socialLinks:"rwtEf8SI5iVgvm1vnHM4H"}}}]);
